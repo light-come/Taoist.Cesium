cec26fc4e8f2522505708dac0eddfc9d45eb5b06
BUG killer :) <38523332+light-come@users.noreply.github.com>
2022-05-19 09:24:19 +0800

    release 0.0.1

--- test/ESCesium/sesium-troke/src/bloom/createUnrealBloomStage.d.ts
+++
@@ -1,9 +0,0 @@
-// import * as createUnrealBloomStage from './createUnrealBloomStage'
-
-/**
- * 在Cesium后期处理
- * @param {string} [name] 
- * @returns {any} stage
- */
-export default function createUnrealBloomStage(name?: string,kernelSizeArray?:any): any
-// export default createUnrealBloomStage
